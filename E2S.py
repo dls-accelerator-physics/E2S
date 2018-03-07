c59cc8897a861d33fbc008be2051db536e811545
marlibgin <marco.apollonio@diamond.ac.uk>
2018-03-07 23:45:47 +0000

    modified to use the beam-moments

--- E2S.py
+++ E2S.py
@@ -47,270 +47,228 @@ from fct_get_SR_param                  import DisplayCirc
 
 from fct_get_beam_param_from_twiss     import GetBeamParam
 
-# def twiss_at_location(fichier,locpoint):
 
-#     import glob
-#     import os
-#     import pandas as pd
-#     import matplotlib.pyplot as plt
-#     import sys
-#     import subprocess
+def read_input(filin):
+    INPUT_file = filin # e.g. 'E2S.input'
+    infile     = open(INPUT_file,'r')
 
 
-#     os.system(' pwd ')
+    variables =[]; values=[];
+    for line in infile:
+        variable, value = line.split('=')
+        variable = variable.strip()  # remove leading/traling blanks
+        value    = value.strip()
+        variables.append(variable)
+        values.append(value)
 
-#     print('reading the position s argument...')
-#     #locpoint=float(sys.argv[2])
+    infile.close()
 
-#     print('position read...')
-
-
-# ###############################################################################
-#     def closest(list, Number):
-#         aux = []
-#         for valor in list:
-#             aux.append(abs(Number-valor))
-# 
-#         return aux.index(min(aux))
-# ###############################################################################
-
-
-#     #fichier=sys.argv[1]
-
-#     cmdstring =("sddsprintout -spreadsheet=csv -col=* %s temp_table01234567.csv" %fichier)
-#     os.system(cmdstring)
-
-
-#     thisdir=os.getcwd()
-
-#     df=pd.read_csv(os.path.join(thisdir,'temp_table01234567.csv'))
-#     dft=df[['s','betax','alphax','betay','alphay','etax','etaxp','ElementName','ElementType']]
-
-
-#     bb=closest(dft['s'],locpoint)
-
-
-#     print("closest s to the given location      : ",dft.loc[bb,'s'])
-#     print("index of this value in the twiss file: ", bb)
-#     print("betax     :", dft.loc[bb,'betax'])
-#     print("alphax    :", dft.loc[bb,'alphax'])
-#     print("betay     :", dft.loc[bb,'betay'])
-#     print("alphay    :", dft.loc[bb,'alphay'])
-#     print("etax     :", dft.loc[bb,'etax'])
-#     print("etaxp    :", dft.loc[bb,'etaxp'])
-#     print('data retrieved. process completed.')
-
-
-#     os.system('rm temp_table01234567.csv')
-#     return [dft.loc[bb,'betax'], dft.loc[bb,'alphax'], dft.loc[bb,'betay'], dft.loc[bb,'alphay'], dft.loc[bb,'etax'], dft.loc[bb,'etaxp']]
-
-
-#
-# read main steering file
-# E2S.input with information about:
-# - ID to process
-#   . ID identification       (e.g. I20)
-#   . Np_und:  n. of periods  (e.g. 
-#   . lam_und: period 
-#   . By_und:  max By
-#   
-# - SR global parameters
-#   . Ee: Beam Energy
-#   . Ib: Beam Current
-#   . Circ: SR length
-#   . Nbunch: n. of bunches
-#
-# - Elegant
-#   . LATTICE: lattice name 
-#
-
-
-# ele_input_filnam = 'ele.input'
-
-INPUT_file = 'E2S.input'
-
-infile     = open(INPUT_file,'r')
-
-
-variables =[]; values=[];
-for line in infile:
-    variable, value = line.split('=')
-    variable = variable.strip()  # remove leading/traling blanks
-    value    = value.strip()
-    variables.append(variable)
-    values.append(value)
-
-infile.close()
-
-dict={}              # create a dictionary for easy access to variables 
+    dict={}              # create a dictionary for easy access to variables 
  
-for i in range(0,len(variables)) :
-    dict[variables[i]] = values[i]
+    for i in range(0,len(variables)) :
+        dict[variables[i]] = values[i]
 
+    dict['INPUT_file'] = INPUT_file
+    return dict
+
+def e2s(dict):
+    
 #*********** ID
-By_und  = float(dict['By_und'])
-lam_und = float(dict['lam_und'])
-Np_und  = float(dict['Np_und'])
-K_und   = 0.9338 * By_und * lam_und * 100
-IDpos    = float(dict['IDpos'])
+    By_und  = float(dict['By_und'])
+    lam_und = float(dict['lam_und'])
+    Np_und  = float(dict['Np_und'])
+    K_und   = 0.9338 * By_und * lam_und * 100
+    IDpos    = float(dict['IDpos'])
 #*********** SR Parameters
-Ee      = float(dict['Ee'])
-Ib      = float(dict['Ib'])
+    Ee      = float(dict['Ee'])
+    Ib      = float(dict['Ib'])
 # Circ    = float(dict['Circ'])
-Nbunch  = float(dict['Nbunch'])
-
+    Nbunch  = float(dict['Nbunch'])
+    Cou     = float(dict['Cou'])
+    
 #*********** BeamLine Parameters
-slitZ   = float(dict['slitZ'])
-slitDX  = float(dict['slitDX'])
-slitDY  = float(dict['slitDY'])
-Ephot_ini = float(dict['Ephot_ini'])
-Ephot_end = float(dict['Ephot_end'])
-
+    slitZ   = float(dict['slitZ'])
+    slitDX  = float(dict['slitDX'])
+    slitDY  = float(dict['slitDY'])
+    Ephot_ini = float(dict['Ephot_ini'])
+    Ephot_end = float(dict['Ephot_end'])
+    
 #********** Calculation Parameters (SRW)
-outfil    = str(dict['outfil'])
-meshXsta  = float(dict['meshXsta'])
-meshXfin  = float(dict['meshXfin'])
-meshYsta  = float(dict['meshYsta'])
-meshYfin  = float(dict['meshYfin'])
-meshEsta  = float(dict['meshEsta'])
-meshEfin  = float(dict['meshEfin'])
+    outfil    = str(dict['outfil'])
+    meshXsta  = float(dict['meshXsta'])
+    meshXfin  = float(dict['meshXfin'])
+    meshYsta  = float(dict['meshYsta'])
+    meshYfin  = float(dict['meshYfin'])
+    meshEsta  = float(dict['meshEsta'])
+    meshEfin  = float(dict['meshEfin'])
+    
+    IDname   = str(dict['IDname'])
+    LATTICE  = str(dict['LATTICE'])
+    calc_type = str(dict['calc_type'])
+    calc_meth = str(dict['calc_meth'])  # 0 = manual / 1 = undulator / 2 = wiggler 
 
-IDname   = str(dict['IDname'])
-LATTICE  = str(dict['LATTICE'])
-calc_type = str(dict['calc_type'])
-calc_meth = str(dict['calc_meth'])  # 0 = manual / 1 = undulator / 2 = wiggler 
+# ******* Input file name
+    INPUT_file = dict['INPUT_file']
+
+
 #LATTICE = 'DTBA_C1a_AA'
-
-LATdir  = 'e2s_LATTICES/'
-SRWdir  = 'e2s_SRW/'
-
-
-# ---------------------------------
-# elegant lattice type: LATTICE.lte 
-# ---------------------------------
-
-eLTE = LATTICE+'.lte'
-
-# ----------------------------------
-# elegant steering file: LATTICE.ele 
-# ----------------------------------
-
-here = os.getcwd() # memorize the TOP directory
-
-cmd  = here+'/'+LATdir  # cd to ELEgant directory 
-os.chdir(cmd)
-eELE = LATTICE+'.ele'
-
-# ----------------------------------
-# RUN elelgant
-# ----------------------------------
-
-cmd  = 'elegant '+eELE
-os.system(cmd)
-
-eTWI   = LATTICE+'.twi'
-eRF    = LATTICE+'.rf'
-eMAG   = LATTICE+'.mag'
-
-# ----------------------------------
-# retrieve results from elegant run
-# ----------------------------------
-
+    LATdir  = 'e2s_LATTICES/'
+    SRWdir  = 'e2s_SRW/'
+    
+    
+    # ---------------------------------
+    # elegant lattice type: LATTICE.lte 
+    # ---------------------------------
+    
+    eLTE = LATTICE+'.lte'
+    
+    # ----------------------------------
+    # elegant steering file: LATTICE.ele 
+    # ----------------------------------
+    
+    here = os.getcwd() # memorize the TOP directory
+    
+    cmd  = here+'/'+LATdir  # cd to ELEgant directory 
+    os.chdir(cmd)
+    eELE = LATTICE+'.ele'
+    
+    # ----------------------------------
+    # RUN elelgant
+    # ----------------------------------
+    
+    cmd  = 'elegant '+eELE
+    os.system(cmd)
+    
+    eTWI   = LATTICE+'.twi'
+    eRF    = LATTICE+'.rf'
+    eMAG   = LATTICE+'.mag'
+    
+    # ----------------------------------
+    # retrieve results from elegant run
+    # ----------------------------------
+    
 #spos = 282.298
-s,sIndex,betax,alphax,betay,alphay,etax,etaxp,ex0,Sdelta0 = GetTwissList(eTWI,IDpos)
-Sz0  = GetRF(eRF)
+    s,sIndex,betax,alphax,betay,alphay,etax,etaxp,ex0,Sdelta0 = GetTwissList(eTWI,IDpos)
+    Sz0  = GetRF(eRF)
+    
+    Circ = GetCirc(LATTICE)
+    
+    #cou  = 0.01
+    beam, mom = GetBeamParam([betax,alphax,betay,alphay,etax,etaxp,ex0,Sdelta0,Cou,Sz0])
+    
 
-Circ = GetCirc(LATTICE)
-
-cou  = 0.01
-beam = GetBeamParam([betax,alphax,betay,alphay,etax,etaxp,ex0,Sdelta0,cou,Sz0])
-
-
-print("***************************************************************")
-print("***************************************************************")
-print("closest s to the requested location    : ",s)
-print("index of this value in the twiss file  : ", sIndex)
-print("***************************************************************")
-print("***************************************************************")
-print("                                   ")
-print(" Twiss parameters at that location:")
-print(" ----------------------------------")
-print(" betax     :", betax, " (m)")
-print(" alphax    :", alphax)
-print(" betay     :", betay, " (m)")
-print(" alphay    :", alphay)
-print(" etax      :",  etax, " (m)")
-print(" etaxp     :", etaxp)
-
-print(" sx     :", beam[0], " (m)")
-print(" sy     :", beam[1], " (m)")
-print(" sxp    :", beam[2], " (m)")
-print(" syp    :", beam[3], " (m)")
-
-print("                  ")
-print(" Global parameters:")
-print(" ------------------")
-print(" emix       :", ex0)
-print(" dE/E       :", Sdelta0)
-print(" Circ       :", Circ)
-print("                  ")
-print(" sigma_z(0) :", Sz0)
-
-print("                  ")
-print(" ID:")
-print(" ------------------")
-print(" ID name       :", IDname)
-print(" Np_und        :", Np_und)
-
-
-
-
-tgt  = here+'/'+SRWdir
-cmd  = 'cp '+here+'/'+INPUT_file+'  /'+tgt+'/SRW.input'
-os.system(cmd)
-cmd  = tgt
-os.chdir(cmd)
-os.system('echo Circ     = '+str(Circ)+' >> SRW.input\n')
-os.system('echo sig_z    = '+str(Sz0)+' >> SRW.input\n')
-os.system('echo dE       = '+str(Sdelta0)+' >> SRW.input\n')
-os.system('echo -----Beam Twiss/Size:  =  >> SRW.input\n')
-os.system('echo emi_x    = '+str(ex0)+' >> SRW.input\n')
-os.system('echo beta_x   = '+str(betax)+' >> SRW.input\n')
-os.system('echo alpha_x  = '+str(alphax)+' >> SRW.input\n')
-os.system('echo beta_y   = '+str(betay)+' >> SRW.input\n')
-os.system('echo alpha_y  = '+str(alphay)+' >> SRW.input\n')
-os.system('echo eta_x    = '+str(etax)+' >> SRW.input\n')
-os.system('echo eta_xp   = '+str(etaxp)+' >> SRW.input\n')
-os.system('echo sig_x    = '+str(beam[0])+' >> SRW.input\n')
-os.system('echo sig_y    = '+str(beam[1])+' >> SRW.input\n')
-os.system('echo sig_xp   = '+str(beam[2])+' >> SRW.input\n')
-os.system('echo sig_yp   = '+str(beam[3])+' >> SRW.input\n')
-os.system('echo calc_meth   = '+str(calc_meth)+' >> SRW.input\n') # 0 =manual / 1 =undulator / 2 =wiggler
-cmd  = here
-os.chdir(cmd)
+    print("***************************************************************")
+    print("closest s to the requested location    : ",s)
+    print("index of this value in the twiss file  : ", sIndex)
+    print("***************************************************************")
+    print("                                   ")
+    print(" Twiss parameters at that location:")
+    print(" ----------------------------------")
+    print(" betax     :", betax, " (m)")
+    print(" alphax    :", alphax)
+    print(" betay     :", betay, " (m)")
+    print(" alphay    :", alphay)
+    print(" etax      :",  etax, " (m)")
+    print(" etaxp     :", etaxp)
+    
+    print(" sx     :", beam[0], " (m)")
+    print(" sy     :", beam[1], " (m)")
+    print(" sxp    :", beam[2], " (m)")
+    print(" syp    :", beam[3], " (m)")
+    
+    print("                  ")
+    print(" Global parameters:")
+    print(" ------------------")
+    print(" emix       :", ex0)
+    print(" dE/E       :", Sdelta0)
+    print(" Circ       :", Circ)
+    print("                  ")
+    print(" sigma_z(0) :", Sz0)
+    
+    print("                  ")
+    print(" ID:")
+    print(" ------------------")
+    print(" ID name       :", IDname)
+    print(" Np_und        :", Np_und)
+    
+    
+    
+    
+    tgt  = here+'/'+SRWdir
+    cmd  = 'cp '+here+'/'+INPUT_file+'  /'+tgt+'/SRW.input'
+    os.system(cmd)
+    cmd  = tgt
+    os.chdir(cmd)
+    os.system('echo Circ     = '+str(Circ)+' >> SRW.input\n')
+    os.system('echo sig_z    = '+str(Sz0)+' >> SRW.input\n')
+    os.system('echo dE       = '+str(Sdelta0)+' >> SRW.input\n')
+    os.system('echo -----Beam Twiss/Size/Moments:  =  >> SRW.input\n')
+    os.system('echo emi_x    = '+str(ex0)+' >> SRW.input\n')
+    os.system('echo beta_x   = '+str(betax)+' >> SRW.input\n')
+    os.system('echo alpha_x  = '+str(alphax)+' >> SRW.input\n')
+    os.system('echo beta_y   = '+str(betay)+' >> SRW.input\n')
+    os.system('echo alpha_y  = '+str(alphay)+' >> SRW.input\n')
+    os.system('echo eta_x    = '+str(etax)+' >> SRW.input\n')
+    os.system('echo eta_xp   = '+str(etaxp)+' >> SRW.input\n')
+    os.system('echo sig_x    = '+str(beam[0])+' >> SRW.input\n')
+    os.system('echo sig_y    = '+str(beam[1])+' >> SRW.input\n')
+    os.system('echo sig_xp   = '+str(beam[2])+' >> SRW.input\n')
+    os.system('echo sig_yp   = '+str(beam[3])+' >> SRW.input\n')
+    os.system('echo sigXX    = '+str(mom[0])+'  >> SRW.input\n')
+    os.system('echo sigXXp   = '+str(mom[1])+'  >> SRW.input\n')
+    os.system('echo sigXpXp  = '+str(mom[2])+'  >> SRW.input\n')
+    os.system('echo sigYY    = '+str(mom[3])+'  >> SRW.input\n')
+    os.system('echo sigYYp   = '+str(mom[4])+'  >> SRW.input\n')
+    os.system('echo sigYpYp  = '+str(mom[5])+'  >> SRW.input\n')
+    os.system('echo calc_meth   = '+str(calc_meth)+' >> SRW.input\n') # 0 =manual / 1 =undulator / 2 =wiggler
+    cmd  = here
+    os.chdir(cmd)
 
 
-# ----------------------------------
-# Run SRW
-# ----------------------------------
-
-cmd  = here+'/'+SRWdir  # cd to ELEgant directory 
-os.chdir(cmd)
-
+    # ----------------------------------
+    # Run SRW
+    # ----------------------------------
+    
+    cmd  = here+'/'+SRWdir  # cd to ELEgant directory 
+    os.chdir(cmd)
+    
 #os.system('python SRW_I13d_individual_electrons.py SRW.input')
 ###### os.system('./submit_runbatch_Individual.sh')
-print("Calc Type is "+calc_type)
-if calc_type == 'individual':
-    print("INTENSITY CALCULATION - individual front calculation")
-    os.system('./submit_runbatch_Individual.sh')
-elif calc_type == 'multie':
-    print("INTENSITY CALCULATION - multi-e mode") 
-    os.system(' /dls_sw/apps/python/anaconda/1.7.0/64/bin/python SRW_I13d_intensity.py SRW.input')
-elif calc_type == 'flux':
-    print("FLUX CALCULATION")
+    print("Calc Type is "+calc_type)
+    if calc_type == 'individual':
+        print("INTENSITY CALCULATION - individual front calculation")
+        os.system('./submit_runbatch_Individual.sh')
+    
+    elif calc_type == 'multie':
+        print("INTENSITY CALCULATION - multi-e mode") 
+        #os.system(' /dls_sw/apps/python/anaconda/1.7.0/64/bin/python SRW_I13d_intensity.py SRW.input')
+        os.system(' /dls_sw/apps/python/anaconda/1.7.0/64/bin/python SRW_intensity.py SRW.input')
+    
+    elif calc_type == 'flux':
+        print("FLUX CALCULATION - interactive mode ... ")
+        os.system(' /dls_sw/apps/python/anaconda/1.7.0/64/bin/python SRW_flux.py SRW.input')
+    
+    elif calc_type == 'flux_cluster':
+        print("FLUX CALCULATION - using the cluster ... ")
     #os.system(' /dls_sw/apps/python/anaconda/1.7.0/64/bin/python SRW_I13d_flux.py SRW.input')
-    os.system(' /dls_sw/apps/python/anaconda/1.7.0/64/bin/python SRW_flux.py SRW.input')
+        #### os.system(' /dls_sw/apps/python/anaconda/1.7.0/64/bin/python SRW_flux.py SRW.input')
+        os.system('./submit_runbatch_Flux.sh')
+    
+        
+        cmd = here
+        os.chdir(cmd)
+        
 
 
-cmd = here
-os.chdir(cmd)
+def main():
+    filin = sys.argv[1]
+    print(filin)
+    dict = read_input(filin)
+    print(dict)
+    e2s( dict )
+
+
+if __name__ == '__main__':
+    main()
+
+    
