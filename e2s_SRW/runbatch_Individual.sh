d4c9d656663e331f8af6c77ab077d0e84b2ca65b
marlibgin <marco.apollonio@diamond.ac.uk>
2018-08-14 00:13:10 +0100

    (submit_)runbatch_Individual.sh: proper use of openmpi/mpiexec to submit parallel jobs

--- e2s_SRW/runbatch_Individual.sh
+++ e2s_SRW/runbatch_Individual.sh
@@ -1,6 +1,11 @@
 #
 # memo-script to run SRWLIB_individualelectrons.py in BATCH mode 
-# on the cluster
+# on the cluster. 
 #
-/dls_sw/apps/python/anaconda/1.7.0/64/bin/python SRW_individualelectrons.py SRW.input
+# it makes use of mpiexec as discussed with M.Furseman, and it is launched 
+# by submit_runbatch_Individual.sh 
+# MA 13/8/2018
+#
+##/dls_sw/apps/python/anaconda/1.7.0/64/bin/python SRW_individualelectrons.py SRW.input
+mpiexec /dls_sw/apps/python/anaconda/1.7.0/64/bin/python SRW_individualelectrons.py SRW.input
 
--- e2s_SRW/submit_runbatch_Individual.sh
+++ e2s_SRW/submit_runbatch_Individual.sh
@@ -1,3 +1,11 @@
+#
+# memo-script to run SRWLIB_individualelectrons.py in BATCH mode 
+# on the cluster. 
+#
+# submit_runbatch_Individual.sh uses openmpi with a defined number of nodes
+# to start a batch session using runbatch_Individual.sh
+# MA 13/8/2018
+#
 #qsub -q ap-medium.q -l redhat_release=rhel6 runbatch_Individual.sh
 qsub -q ap-high.q -l redhat_release=rhel6 -V -pe openmpi 50 runbatch_Individual.sh
 
